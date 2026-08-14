61e1f48520bebaa8c8d53fecb9c0320a6ba0c98a
vxyxqor <vxyxqor@gmail.com>
2026-08-14 13:03:21 +0800

    Add Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'static',
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   site: 'https://vxyxqor.com',
 });
